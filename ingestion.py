1f3477b992246fbb8bd0b8a652baa0579b83ddae
Umme Kulsum <kulsum0805@gmail.com>
2026-03-16 16:51:53 +0530

    fix langchain text splitter import

--- ingestion.py
+++ ingestion.py
@@ -1,7 +1,7 @@
 # ingestion.py
 import os
 from langchain_community.document_loaders import PyPDFLoader, TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
 def load_documents(folder_path: str):
